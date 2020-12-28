002ef0dd1d8922f9d872308e2dadb437737d7ff0
EddieJorden <74274857+EddieJorden@users.noreply.github.com>
2020-12-28 15:23:12 -0800

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import profileImage from './img/me in woods.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My new React App</h1>
+      <h2>EddieJorden.tech</h2>
+      <img src={profileImage} alt="me in Woods"/>
     </div>
   );
 }
