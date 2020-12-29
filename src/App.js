5762e175938be096f60200d4329299ac3eae5431
EddieJorden <74274857+EddieJorden@users.noreply.github.com>
2020-12-28 20:45:34 -0800

    Stylingchanges (#1)
    * um i staged and commited this time??
    
    * i changed something
    
    * changed height on image

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import profileImage from './img/me in woods.jpg';
 import './App.css';
-
-
 function App() {
+  const headerStyles = {fontSize: "24px", fontWeight: "strong"}
+  const headerContainerStyle = {height: "100px", justifyContent: "center", display: "flex", flexDirection: "column"}
   return (
     <div className="App">
-      <h1>My new React App</h1>
-      <h2>EddieJorden.tech</h2>
-      <img  src={profileImage} />
-
+      <div style={headerContainerStyle}>
+        <div style={headerStyles}>My new React App</div>
+        <div style={headerStyles}>EddieJorden.tech</div>
+      </div>
+      <img  className="home-page-image" src={profileImage} alt=""/>
     </div>
   );
 }
-
 export default App;
